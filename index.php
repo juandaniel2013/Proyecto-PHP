af37974b35d97ba5fd6b7f76509859c93ecbd74d
daniel <mydanielsanchez@gmail.com>
2019-08-24 02:43:49 -0400

    iniciando archivos

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang = "en">
+	<head>
+		<?php include 'cabecera.php';?>
+		<link rel="stylesheet" href="estilo.css">
+	</head>
+	<body>
+		<?php include 'navegacion.php';?>
+
+		<script src="app.js"></script>	
+		<?php include 'pie.php';?>
+	</body>
+</html>
---
+++ pie.php
@@ -0,0 +1,10 @@
+<?php 
+echo 
+'<footer>
+	<div>
+		<p>Aplicación de Ciencias de la Computación</p>
+		<p>Cochabamba - Bolivia</p>
+		<p>&copy; Todos los derechos reservados</p> 
+	</div>
+</footer>'
+?>
